516ccff30de2b6388c7f54fea8ec93d6a4c9b641
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-06-14 09:25:44 +0600

    changes made

--- src/Componenets/HomePage/Banner/Banner.js
+++ src/Componenets/HomePage/Banner/Banner.js
@@ -1,7 +1,38 @@
 import React from "react";
+import bannerImage from "../../../Images/Banner.jpg";
+
+import "./Banner.css";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div>
+            <img
+              className="mask mask-decagon"
+              src={bannerImage}
+              alt="No image Found"
+            />
+          </div>
+
+          <div>
+            <h1 class="text-5xl font-bold">
+              Discover Restaurant <br /> & Delicious Food
+            </h1>
+            <p class="py-6">
+              <input
+                type="text"
+                placeholder="Search Restaurant And Foods"
+                class="input input-bordered input-secondary w-full max-w-xs"
+              />
+              <button class="btn bg-[#FE043C]">Go</button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
 
     <div class="navbar bg-base-100">
       <div class="navbar-start">
-        <div class="dropdown">
+        {/* <div class="dropdown">
           <label tabindex="0" class="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -53,11 +53,35 @@ const Header = () => {
           >
             {menuItems}
           </ul>
-        </div>
+        </div> */}
         <h1 class=" font-bold mx-14 text-5xl text-[#FE043C]	">expice</h1>
       </div>
       <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          {menuItems}
+        </ul>
       </div>
     </div>
   );
