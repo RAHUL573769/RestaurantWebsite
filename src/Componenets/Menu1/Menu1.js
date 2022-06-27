dc231bb5fc41c4a4a83d1eb299467efdbcce14e9
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-06-27 09:48:48 +0600

    menu added

--- src/Componenets/Menu1/Menu1.js
+++ src/Componenets/Menu1/Menu1.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import SingleMenu1 from "./SingleMenu1";
 
 const Menu1 = () => {
+  const [menu1, setMenu1] = useState();
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+      .then((res) => res.json())
+      .then((data) => setMenu1(data));
+  }, []);
+
+  console.log(menu1?.meals.length);
   return (
-    <div>
-      <h1>Hi---------------------------------------------------------</h1>
+    <div class="grid   grid-cols-3">
+      {menu1?.meals?.map((meal) => (
+        <SingleMenu1 meal={meal}></SingleMenu1>
+      ))}
     </div>
   );
 };
---
+++ src/Componenets/Menu1/SingleMenu1.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SingleMenu1 = ({ meal }) => {
+  console.log(meal);
+  return (
+    <div>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src={meal.strMealThumb} alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">{meal.strMeal}</h2>
+          <p>Meal Id :{meal.idMeal}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleMenu1;
