e0d14235868eb951b1ba783969b8a1387e6b53d6
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-07-23 22:18:17 +0600

    finally commit

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -40,30 +40,6 @@ const Header = () => {
 
     <div class="navbar bg-base-100">
       <div class="navbar-start">
-        {/* <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {menuItems}
-          </ul>
-        </div> */}
         <h1 class=" font-bold mx-14 text-5xl text-[#FE043C]	">
           <a href="https://www.tripadvisor.com/Restaurants-g319837-Chittagong_City_Chittagong_Division.html">
             {" "}
